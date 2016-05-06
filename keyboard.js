c083fe4fedeffc8386c52e9610bfb2e2ee3b4bcc
Changbai Li <changbai.li@heliosinteractive.com>
2016-05-06 13:57:59 -0700

    destroy function to unbind and remove element

--- keyboard.js
+++ keyboard.js
@@ -3,6 +3,7 @@
 function Keyboard(){
 
   this.focusedEl = null;
+  this.keyboardEl = null;
 
   // We would like to pipe all keyboard events through one handler
   var handleKeyboardEvents = function (e) {
@@ -20,9 +21,18 @@ function Keyboard(){
       this.focusedEl.value = text.substring(0, start) + keyInfo.symbol + text.substring(end);
       this.focusedEl.setSelectionRange(start+1, start+1);
     }
-    // Invoke the hook with key's data if there's one
 
   }.bind(this);
+
+  // Generate keyboard HTML, bind events, insert them to given element
+  this.placeIn = function (el) {
+    this.keyboardEl = createKeyboardElement();
+    // bind events
+    this.keyboardEl.addEventListener("mousedown", handleKeyboardEvents);
+    this.keyboardEl.addEventListener("touchstart", handleKeyboardEvents);
+    // Append keys to el
+    el.appendChild(this.keyboardEl);
+  };
 
   function createKeyboardElement() {
     var container = document.createElement("div");
@@ -40,19 +50,10 @@ function Keyboard(){
     return container;
   };
 
-  // Generate keyboard HTML, bind events, insert them to given element
-  this.placeIn = function (el) {
-    var keyboardEl = createKeyboardElement();
-    // bind events
-    keyboardEl.addEventListener("mousedown", handleKeyboardEvents);
-    keyboardEl.addEventListener("touchstart", handleKeyboardEvents);
-    // Append keys to el
-    el.appendChild(keyboardEl);
-  }
+
 
   // Control which el is being updated
   this.focus = function (el) {
-    console.log("focus", el);
     this.focusedEl = el;
   };
   this.blur = function () {
@@ -61,6 +62,14 @@ function Keyboard(){
 
   // Unbind the events, destroy the elements
   this.destroy = function () {
-    console.log("DESTRUCTION");
+    if (this.keyboardEl === null) {
+      console.error("Keyboard.destroy() error: Keyboard HTML Elements aren't present");
+      return;
+    }
+    this.keyboardEl.removeEventListener("mousedown", handleKeyboardEvents);
+    this.keyboardEl.removeEventListener("touchstart", handleKeyboardEvents);
+    this.keyboardEl.remove();
+    this.keyboardEl = null;
+    this.blur();
   };
 };
--- readme.md
+++ readme.md
@@ -18,10 +18,14 @@
   - [x] Make it work w/ 3 buttons first
 - [x] Whenever a key is clicked or touched, it would fire an event that keyboard handles
 - [x] Converge all events to a single handler
+- [ ] Expose a hook for the events
+  - What's the hook used for?
+  - It's part of the decorator pattern, in case we need the keys to do something additional?
+  - Better model being callback functions for some controls?
 - [x] Keyboard can keep track of which element it should update
 - [x] When there is an element the keyboard's focusing on, pressing keys inserts value to it
 - [x] Value inserting works just like actual keyboard, takes caret place and selection into consideration
-- [ ] Destroy function to unbind and destroy html
+- [x] Destroy function to unbind and destroy html
 - [ ] Layouts are defined by config json file and can be swapped
 - [ ] Some keys can have behaviors overriden and run custom functions instead
 - [ ] Generated keys have a structure that allows easy style changing via CSS
