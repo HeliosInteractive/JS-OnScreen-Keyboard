fbb194d87c98c0aa9ae02f0ee03ad00bb0b561ed
Changbai Li <changbai.li@heliosinteractive.com>
2016-05-17 13:18:09 -0700

    prototype some methods

--- keyboard.js
+++ keyboard.js
@@ -1,102 +1,94 @@
 if( !exports ) var exports = {};
 (function(exports, global){
-
   "use strict";
 
   function Keyboard(layoutName){
 
-    if (!layoutName) throw new Error("Keyboard Initiation: missing layout name");
-
-    var activeLayoutName;
-    this.setLayout = function(pName) {
-      if (!Keyboard.layout[pName]) throw new Error("Keyboard layout not found: " + pName);
-      activeLayoutName = pName;
-    }
-
-    this.setLayout(layoutName);
+    if (!Keyboard.layout[layoutName]) throw new Error("keyboard initiation: Missing layout: " + layoutName);
+    this.activeLayoutName = layoutName;
 
     this.focusedEl = null;
     this.keyboardEl = null;
+  };
+  // We would like to pipe all keyboard events through one handler
+  var handleKeyboardEvents = function (e) {
+    e.preventDefault();
+    // Check to make sure it's a key that's pressed
+    if (!e.target.classList.contains("keyboard-key") || !this.focusedEl) return;
+    // Pipe key's data to an object
+    var keyInfo = Object.assign({}, e.target.dataset);
+    // Update focused element if there is one
+    var text = this.focusedEl.value;
+    this.focusedEl.value = text.substring(0, this.focusedEl.selectionStart) +
+        keyInfo.symbol +
+        text.substring(this.focusedEl.selectionEnd);
+    var newCaretPosition = this.focusedEl.value.length - (text.length - this.focusedEl.selectionEnd);
+    this.focusedEl.setSelectionRange(newCaretPosition, newCaretPosition);
+  };
+  // Generate keyboard HTML, bind events, insert them to given element
+  Keyboard.prototype.placeIn = function (el) {
 
-    // We would like to pipe all keyboard events through one handler
-    var handleKeyboardEvents = function (e) {
-      e.preventDefault();
-      // Check to make sure it's a key that's pressed
-      if (!e.target.classList.contains("keyboard-key")) return;
-      // Pipe key's data to an object
-      var keyInfo = Object.assign({}, e.target.dataset);
+    var self = this;
+    if (!Keyboard.layout[self.activeLayoutName]) throw new Error("Keyboard.placeIn(): Missing layout: " + self.activeLayoutName);
+    // Create container
+    self.keyboardEl = document.createElement("div");
+    self.keyboardEl.classList.add("keyboard-container");
+    // Create elements based off layout
 
-      // Update focused element if there is one
-      if (this.focusedEl !== null) {
-        var start = this.focusedEl.selectionStart;
-        var end = this.focusedEl.selectionEnd;
-        var text = this.focusedEl.value;
-        this.focusedEl.value = text.substring(0, start) + keyInfo.symbol + text.substring(end);
-        var newCaretPosition = this.focusedEl.value.length - (text.length - end);
-        this.focusedEl.setSelectionRange(newCaretPosition, newCaretPosition);
+    function foreachLayout(row, rowIndex, layout){
+
+      var rowEl = document.createElement("div");
+      rowEl.classList.add("keyboard-row");
+      rowEl.classList.add("keyboard-row--" + rowIndex);
+
+      function foreachRow(key, keyIndex, row){
+        var keyEl = document.createElement("div");
+        keyEl.classList.add("keyboard-key");
+        keyEl.classList.add("keyboard-key--" + keyIndex);
+        // Parse the layout configuration
+        for (var dataName in key) {
+          switch (dataName) {
+            case "symbol":
+              if (!key.label) keyEl.innerHTML = key[dataName];
+              keyEl.dataset.symbol = key[dataName];
+              break;
+            case "label":
+              keyEl.innerHTML = key[dataName];
+              break;
+            default:
+              keyEl.dataset[dataName] = key[dataName];
+          }
+        }
+        rowEl.appendChild(keyEl);
       }
 
-    }.bind(this);
+      row.forEach(foreachRow);
+      self.keyboardEl.appendChild(rowEl);
+    }
 
-    // Generate keyboard HTML, bind events, insert them to given element
-    this.placeIn = function (el) {
-      if (!Keyboard.layout[activeLayoutName]) throw new Error("Keyboard.placeIn(): Missing layout: " + activeLayoutName);
-      // Create container
-      this.keyboardEl = document.createElement("div");
-      this.keyboardEl.classList.add("keyboard-container");
-      // Create elements based off layout
-      // TODO There's a lot of indentation here.
-      Keyboard.layout[activeLayoutName].forEach(function(row, rowIndex, layout){
-        var rowEl = document.createElement("div");
-        rowEl.classList.add("keyboard-row");
-        rowEl.classList.add("keyboard-row--" + rowIndex);
-        row.forEach(function(key, keyIndex, row){
-          var keyEl = document.createElement("div");
-          keyEl.classList.add("keyboard-key");
-          keyEl.classList.add("keyboard-key--" + keyIndex);
-          // Parse the layout configuration
-          for (var dataName in key) {
-            switch (dataName) {
-              case "symbol":
-                if (!key.label) keyEl.innerHTML = key[dataName];
-                keyEl.dataset.symbol = key[dataName];
-                break;
-              case "label":
-                keyEl.innerHTML = key[dataName];
-                break;
-              default:
-                keyEl.dataset[dataName] = key[dataName];
-            }
-          }
-          rowEl.appendChild(keyEl);
-        });
-        this.keyboardEl.appendChild(rowEl);
-      }, this);
+    Keyboard.layout[self.activeLayoutName].forEach(foreachLayout);
+    // bind events
+    self.keyboardEl.addEventListener("mousedown", handleKeyboardEvents.bind(self));
+    self.keyboardEl.addEventListener("touchstart", handleKeyboardEvents.bind(self));
+    // Append keys to el
+    el.appendChild(self.keyboardEl);
+  };
 
-      // bind events
-      this.keyboardEl.addEventListener("mousedown", handleKeyboardEvents);
-      this.keyboardEl.addEventListener("touchstart", handleKeyboardEvents);
-      // Append keys to el
-      el.appendChild(this.keyboardEl);
-    };
-
-    // Control which el is being updated
-    this.focus = function (el) {
-      this.focusedEl = el;
-    };
-    this.blur = function () {
-      this.focusedEl = null;
-    };
-
-    // Unbind the events, destroy the elements
-    this.destroy = function () {
-      if (this.keyboardEl === null) throw new Error("Keyboard.destroy() error: Keyboard HTML Elements aren't present");
-      this.keyboardEl.removeEventListener("mousedown", handleKeyboardEvents);
-      this.keyboardEl.removeEventListener("touchstart", handleKeyboardEvents);
-      this.keyboardEl.remove();
-      this.keyboardEl = null;
-      this.blur();
-    };
+  // Control which el is being updated
+  Keyboard.prototype.focus = function (el) {
+    this.focusedEl = el;
+  };
+  Keyboard.prototype.blur = function () {
+    this.focusedEl = null;
+  };
+  // Unbind the events, destroy the elements
+  Keyboard.prototype.destroy = function () {
+    if (this.keyboardEl === null) throw new Error("Keyboard.destroy() error: Keyboard HTML Elements aren't present");
+    this.keyboardEl.removeEventListener("mousedown", handleKeyboardEvents.bind(this));
+    this.keyboardEl.removeEventListener("touchstart", handleKeyboardEvents.bind(this));
+    this.keyboardEl.remove();
+    this.keyboardEl = null;
+    this.blur();
   };
 
   Keyboard.layout = {};
